e402e9ca98f234ff9ede0cb3380f565065f194e5
strbytes <strbytes@gmail.com>
2022-04-15 20:08:30 -0400

    Add JS selectors for buttons
    Also move basic operators into an object in order to use bracket
    member selection by a variable.

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const add = (x, y) => x + y;
-const sub = (x, y) => x - y;
-const mul = (x, y) => x * y;
-const div = (x, y) => x / y;
+const nums = document.querySelectorAll(".numbers");
+const ops = document.querySelectorAll(".operators");
+const sign = document.querySelector("#sign");
+const decimal = document.querySelector("#decimal");
+
+const operators = {
+  add: (x, y) => x + y,
+  sub: (x, y) => x - y,
+  mul: (x, y) => x * y,
+  div: (x, y) => x / y,
+  pct: (x) => x * 0.01,
+};
 
 function operate(operator, x, y) {
   return operator(x, y);
