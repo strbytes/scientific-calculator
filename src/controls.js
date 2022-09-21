56c021f4f5e4ba2d76150aee85969a3f9f7f1907
strbytes <strbytes@gmail.com>
2022-09-21 16:24:52 -0400

    Rework controls.js to be more comprehensive
    All buttons are now created dynamically, organized into sections, and
    contain information about the 2nd option for relevant keys. Output is
    currently unstyled.

--- src/controls.js
+++ src/controls.js
@@ -1,41 +1,139 @@
-const controlsDiv = document.querySelector("#controls");
-const numbers = [
-  "7", "8", "9", "4", "5", "6", "1", "2", "3", "-", "0", ".",
-]
-const binary = ["^", "*", "/", "+", "-"]
-const unary = ["sqrt", "log"]
-const constants = ["pi", "e"]
+const controls = {
+  edit: [
+    {
+      label: "del",
+      second: ""
+    },
+    {
+      label: "<-",
+      second: "",
+      id: "back"
+    },
+    {
+      label: "->",
+      second: "",
+      id: "forward"
+    },
+  ],
+
+  left: [
+    {
+      id: "second",
+      label: "2nd",
+      second: ""
+    },
+    {
+      label: "log",
+      second: "10^"
+    },
+    {
+      label: "ln",
+      second: "e^"
+    },
+    {
+      label: "pi",
+      second: "e"
+    },
+    {
+      label: "^",
+      second: "x\u221A"
+    },
+    {
+      label: "x\u00B2",
+      second: "\u221A"
+    },
+  ],
+
+  top: [
+    {
+      label: "sin",
+      second: "sin\u207B"
+    },
+    {
+      label: "cos",
+      second: "cos\u207B"
+    },
+    {
+      label: "tan",
+      second: "tan\u207B"
+    },
+    {
+      label: "x\u207B\u00B9",
+      second: "\u1D07"
+    },
+    {
+      label: "(",
+      second: ""
+    },
+    {
+      label: ")",
+      second: ""
+    },
+  ],
+
+  right: [
+    {
+      label: "clear",
+      second: ""
+    },
+    {
+      label: "/",
+      second: ""
+    },
+    {
+      label: "*",
+      second: ""
+    },
+    {
+      label: "-",
+      second: ""
+    },
+    {
+      label: "+",
+      second: ""
+    },
+    {
+      label: "=",
+      second: ""
+    },
+  ],
+
+  numbers: [
+    {label: "7", second: ""}, {label: "8", second: ""},{label: "9", second: ""},
+    {label: "4", second: ""}, {label: "5", second: ""},{label: "6", second: ""},
+    {label: "1", second: ""}, {label: "2", second: ""},{label: "3", second: ""},
+    {label: "-", second: ""}, {label: "0", second: ""},{label: ".", second: ""},
+  ]
+}
 
 function buildControls() {
-  controlsDiv.appendChild(buildSection("numbers", numbers, 4));
-}
-
-function buildSection(sectionLabel, buttonLabels, rows) {
-  const columns = buttonLabels.length / rows;
-  console.assert(columns % 1 === 0);
-  const sectionButtons = document.createElement("div");
-  sectionButtons.id = sectionLabel;
-
-  for (let r = 0; r < rows; r++) {
-    let rowDiv = document.createElement("div");
-
-    for (let c = 0; c < buttonLabels.length / rows; c++) {
-      let button = makeButton(buttonLabels[r * columns + c]);
-      rowDiv.appendChild(button);
-    }
-    
-    sectionButtons.appendChild(rowDiv);
+  const controlsDiv = document.querySelector("#controls");
+  for (let section in controls) {
+    controlsDiv.appendChild(buildSection(section));
   }
-  return sectionButtons;
 }
 
+function buildSection(section) {
+  const sectionDiv = document.createElement("div");
+  sectionDiv.id = section;
+  for (let c of controls[section]) {
+    sectionDiv.appendChild(makeButton(c.label, c.second, c.id));
+  }
+  return sectionDiv;
+}
 
-
-function makeButton(label) {
+function makeButton(label, second, id) {
   let button = document.createElement("button");
+  button.id = id? id : label;
   button.textContent = label;
-  button.id = label;
+  button.dataset.second = second;
+  button.addEventListener("click", keyHandler);
   return button;
+}
+
+function keyHandler(e) {
+  // TODO implement buffer
+  buffer.append(e.target.id)
 }
 
 export default buildControls;
