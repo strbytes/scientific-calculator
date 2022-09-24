c3844cbb69c1c478f1b62a1411548f21f691ff16
strbytes <strbytes@gmail.com>
2022-09-24 09:08:09 -0400

    Connect Calculator to screens and fix second bug

--- src/calc/calc.js
+++ src/calc/calc.js
@@ -2,6 +2,8 @@ import InputBuffer from './buffer.js';
 
 class Calculator {
     #buffer = new InputBuffer();
+    #inputScreen = document.querySelector("#input-screen");
+    #outputScreen = document.querySelector("#output-screen");
     #second = false;
     #specialKeys = {
         "equals": this.evaluate(this.#buffer),
@@ -11,22 +13,28 @@ class Calculator {
         "right": this.#buffer.right.bind(this.#buffer),
         "second": _ => this.#second = this.#second? false : true,
     }
-    
+
     evaluate(b) {
         // TODO
     }
 
     keyHandler(e) {
         let key = e.target;
-        let keyValue = second? key.dataset.secondaryValue : key.id;
-        
+        let keyValue = this.#second? key.dataset.secondaryValue : key.id;
+        let keySymbol = this.#second? key.dataset.secondaryValue : key.id;
+
         if (this.#specialKeys.hasOwnProperty(keyValue)) {
             this.#specialKeys[keyValue]();
+        } else {
+            this.#buffer.add(keyValue);
         }
-        
         if (keyValue !== "second") this.#second = false;
-        console.log(`keyValue=${keyValue}, second=${this.#second}`);
+
+        // TODO update buffer to display symbols somehow
+        // Store it in buttons as a data value?
+        // Store controls as a new module containing all control information
+            // accessed by ControlBuilder and Calculator
+        this.#inputScreen.textContent = this.#buffer.toString();
     }
 }
-
 export default Calculator;
